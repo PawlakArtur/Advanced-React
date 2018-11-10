3157439d845e53858c5455c4e1fcd0d9876b799b
Artur Pawlak <pawlak.artur90@gmail.com>
2018-11-10 14:44:22 +0100

    Add Pagination component for frontend

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,6 +3,7 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
+import Pagination from './Pagination';
 
 const ALL_ITMES_QUERY = gql`
 	query ALL_ITMES_QUERY {
@@ -33,6 +34,7 @@ class Items extends Component {
 	render() {
 		return (
 			<Center>
+				<Pagination page={this.props.page}/>
 				<Query query={ALL_ITMES_QUERY}>
 					{({ data, error, loading }) => {
 						if (loading) return <p>Loading...</p>
@@ -42,6 +44,7 @@ class Items extends Component {
 						</ItemsList>
 					}}
 				</Query>
+				<Pagination page={this.props.page}/>
 			</Center>
 		);
 	}
