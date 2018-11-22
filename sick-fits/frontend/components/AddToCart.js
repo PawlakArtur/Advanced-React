fde448ec1b787edfb27ffd495184366f4285e503
Artur Pawlak <pawlak.artur90@gmail.com>
2018-11-22 21:25:38 +0100

    Update Cart look&feel for frontend

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mutation, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
 	mutation addToCart($id: ID!) {
@@ -17,9 +18,10 @@ class AddToCart extends React.Component {
 		return (
 			<Mutation
 				mutation={ADD_TO_CART_MUTATION}
-				variables={{id}}>
-				{addToCart => (
-					<button onClick={addToCart}>Add To Cart</button>
+				variables={{id}}
+				refetchQueries={[CURRENT_USER_QUERY]}>
+				{(addToCart, { loading}) => (
+					<button onClick={addToCart} disabled={loading}>Add{loading && 'ing'} To Cart</button>
 				)}
 			</Mutation>
 		)
