b57234f768fdf87806e72727c52a7b2dbdb043e3
Artur Pawlak <pawlak.artur90@gmail.com>
2018-11-28 21:28:39 +0100

    Add Cart Count component and animate it for frontend

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,7 +3,9 @@ import { Mutation } from 'react-apollo';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 import { TOGGLE_CART_MUTATION } from './Cart'
+console.log(CartCount);
 
 const Nav = () => (
 	<User>
@@ -27,7 +29,10 @@ const Nav = () => (
 						<Mutation
 							mutation={TOGGLE_CART_MUTATION}>
 							{toggleCart => (
-								<button onClick={toggleCart}>My Cart</button>
+								<button onClick={toggleCart}>
+									My Cart
+									<CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}/>
+								</button>
 							)}
 						</Mutation>
 					</>
