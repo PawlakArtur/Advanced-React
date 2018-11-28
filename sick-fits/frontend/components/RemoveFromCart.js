2d86e350eaf44c826d2c3751d74e682ef3d928f1
Artur Pawlak <pawlak.artur90@gmail.com>
2018-11-28 19:57:20 +0100

    Add update cache and optimistic response to RemoveFromCart component for frontend

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -27,11 +27,31 @@ class RemoveFromCart extends Component {
 	static porpTypes = {
 		id: PropTypes.string.isRequired
 	}
+
+	// This gets called as soon as we get a respone back from the server after a mutation has been performed
+	update = (cache, payload) => {
+		// first read the cache
+		const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+		// remove that item from the cart
+		const cartItemId = payload.data.removeFromCart.id;
+		data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+		// write it back to the cache
+		cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+	}
+
 	render() {
 		return (
 			<Mutation
 				mutation={REMOVE_FROM_CART_MUTATION}
-				variables={{id: this.props.id}}>{
+				variables={{id: this.props.id}}
+				update={this.update}
+				optimisticResponse={{
+					__typename: 'Mutation',
+					removeFromCart: {
+						__typename: 'CartItem',
+						id: this.props.id
+					}
+				}}>{
 				(removeFromCart, { loading, error}) => (
 					<BigButton
 						disabled={loading}
